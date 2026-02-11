2e73b15fc59141a5d897e57aaa294877a6adb644
Brais-Manso <brais.manso@udc.es>
2026-02-11 12:03:55 +0100

    Update ej2.2.jl

--- ej2.2.jl
+++ ej2.2.jl
@@ -1,14 +1,23 @@
 function classifyOutputs(outputs::AbstractArray{<:Real,1}; threshold::Real=0.5)
-    #
-    # Codigo a desarrollar
-    #
+    #Codigo a desarrollar
+    resultado = outputs .>= threshold
+    return resultado 
 end;
 
 function classifyOutputs(outputs::AbstractArray{<:Real,2}; threshold::Real=0.5)
-    #
-    # Codigo a desarrollar
-    #
+    #Codigo a desarrollar
+    if size(outputs, 2)== 1
+        vector= outputs[:]
+        results = classifyOutputs(vector; threshold=threshold)
+        return reshape(results,:,1)
+    else 
+        (_, indicesMaxEachInstance) = findmax(outputs, dims=2)
+        outputsbool= falses(size(outputs))
+        outputsbool[indicesMaxEachInstance] .= true
+        return outputsbool
+    end
 end;
+
 
 function accuracy(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
     #
@@ -33,3 +42,4 @@ function accuracy(outputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,
     # Codigo a desarrollar
     #
 end;
+
