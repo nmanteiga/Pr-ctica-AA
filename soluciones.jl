365b10ef6e1c980e3377188be27d06279e6b7feb
albastp <alba.suatap@gmail.com>
2026-02-15 18:51:15 +0100

    feito o final do 2

--- soluciones.jl
+++ soluciones.jl
@@ -90,8 +90,6 @@ function normalizeZeroMean(dataset::AbstractArray{<:Real,2})
 end;
 
 
-end;
-
 function classifyOutputs(outputs::AbstractArray{<:Real,1}; threshold::Real=0.5)
     #Codigo a desarrollar
     resultado = outputs .>= threshold
@@ -143,23 +141,63 @@ function accuracy(outputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,
 end;
 
 function buildClassANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutputs::Int; transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)))
-    #
-    # Codigo a desarrollar
-    #
+    ann = Chain()
+    numInputsLayer = numInputs
+    
+    for (i, numOutputsLayer) in enumerate(topology)
+        ann = Chain(ann..., Dense(numInputsLayer, numOutputsLayer, transferFunctions[i]))
+        numInputsLayer = numOutputsLayer
+    end
+    
+    if numOutputs == 1
+        #Para a clasificación binaria
+        ann = Chain(ann..., Dense(numInputsLayer, 1, σ))
+    else
+        #Para a clasificación multiclase
+        ann = Chain(ann..., Dense(numInputsLayer, numOutputs), softmax)
+    end
+    
+    return ann
 end;
 
 function trainClassANN(topology::AbstractArray{<:Int,1}, dataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}; transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
-    #
-    # Codigo a desarrollar
-    #
+    inputs, targets = dataset
+    
+    inputs = Float32.(inputs)
+    targets = Float32.(targets)
+    
+    numInputs = size(inputs, 2)
+    numOutputs = size(targets, 2)
+    
+    ann = buildClassANN(numInputs, topology, numOutputs; transferFunctions=transferFunctions)
+    
+    opt_state = Flux.setup(Adam(learningRate), ann)
+    
+    x = inputs'
+    y = targets'
+    
+    #binarycrossentropy para clasificación binaria e crossentropy para clasificación multiclase
+    loss(model, x, y) = (size(y, 1) == 1) ? Losses.binarycrossentropy(model(x), y) : Losses.crossentropy(model(x), y)
+    
+    losses = Float32[]
+    
+    push!(losses, Float32(loss(ann, x, y)))
+    
+    epoch = 0
+    while epoch < maxEpochs && losses[end] > minLoss
+        Flux.train!(loss, ann, [(x, y)], opt_state)
+        push!(losses, Float32(loss(ann, x, y)))
+        epoch += 1
+    end
+    
+    return (ann, losses)
 end;
 
 function trainClassANN(topology::AbstractArray{<:Int,1}, (inputs, targets)::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}}; transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)), maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
-    #
-    # Codigo a desarrollar
-    #
-end;
+    targetsMatrix = reshape(targets, :, 1)
 
+    return trainClassANN(topology, (inputs, targetsMatrix); transferFunctions=transferFunctions, maxEpochs=maxEpochs, minLoss=minLoss, learningRate=learningRate)
+end;
 
 # ----------------------------------------------------------------------------------------------
 # ------------------------------------- Ejercicio 3 --------------------------------------------
