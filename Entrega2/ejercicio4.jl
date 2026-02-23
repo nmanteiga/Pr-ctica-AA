7837b73192698318bee3d683bcad2a9c8bddc868
Ansur López Braña <ansur.lopez.brana@udc.es>
2026-02-23 14:12:34 +0100

    primera función 4

--- Entrega2/ejercicio4.jl
+++ Entrega2/ejercicio4.jl
@@ -4,15 +4,23 @@
 
 
 function confusionMatrix(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
-    #
-    # Codigo a desarrollar
-    #
+    VN = sum((outputs .== 0) .& (targets .== 0))
+    FN = sum((outputs .== 0) .& (targets .== 1))
+    FP = sum((outputs .== 1) .& (targets .== 0))
+    VP = sum((outputs .== 1) .& (targets .== 1))
+    precision = (VN + VP) / (VN + VP + FN + FP)
+    tasa_error = (FP + FN) / (VN + VP + FN + FP)
+    sensibilidad = VP + FN == 0 ? 1 : VP / (VP + FN)
+    especificidad = VN + FP == 0 ? 1 : VN / (VN + FP)
+    vpp = VP + FP == 0 ? 1 : VP / (VP + FP)
+    vpn = VN + FN == 0 ? 1 : VN / (VN + FN)
+    F1 = sensibilidad + vpp == 0 ? 0 : 2 * vpp * sensibilidad / (vpp + sensibilidad)
+    matriz = [VN FP; FN VP]
+    return precision, tasa_error, sensibilidad, especificidad, vpp, vpn, F1, matriz
 end;
 
 function confusionMatrix(outputs::AbstractArray{<:Real,1}, targets::AbstractArray{Bool,1}; threshold::Real=0.5)
-    #
-    # Codigo a desarrollar
-    #
+    
 end;
 
 function confusionMatrix(outputs::AbstractArray{Bool,2}, targets::AbstractArray{Bool,2}; weighted::Bool=true)
