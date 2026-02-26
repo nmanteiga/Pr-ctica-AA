7cec297b9516d942803f85a4b15d46cadcfcbd6b
Brais-Manso <brais.manso@udc.es>
2026-02-26 19:11:37 +0100

    Update ejercicio5.jl

--- Entrega2/ejercicio5.jl
+++ Entrega2/ejercicio5.jl
@@ -6,27 +6,41 @@ using Random
 using Random:seed!
 
 function crossvalidation(N::Int64, k::Int64)
-    #
     # Codigo a desarrollar
-    #
+    vector= collect(1:k)
+    repeats= ceil(Int64, N/k)
+    clonados= repeat(vector, repeats)
+    recortados = clonados[1:N]
+    shuffle!(recortados)
 end;
 
 function crossvalidation(targets::AbstractArray{Bool,1}, k::Int64)
-    #
     # Codigo a desarrollar
-    #
+    indices = zeros(Int64, length(targets))
+    numpositivos= sum(targets)
+    indices[targets] = crossvalidation(numpositivos, k)
+    numnegativos = length(indices) - numpositivos
+    indices[.!targets] = crossvalidation(numnegativos, k)
+    return indices
+    
 end;
 
 function crossvalidation(targets::AbstractArray{Bool,2}, k::Int64)
-    #
     # Codigo a desarrollar
-    #
+    vector = zeros(Int64, size(targets,1))
+    for i in 1:size(targets,2)
+        columna = targets[:, i]
+        numpositivos= sum(columna)
+        vector[columna] = crossvalidation(numpositivos, k)
+    end
+    return vector
 end;
 
 function crossvalidation(targets::AbstractArray{<:Any,1}, k::Int64)
-    #
     # Codigo a desarrollar
-    #
+    matriz_booleana = oneHotEncoding(targets)
+    resultado =crossvalidation(matriz_booleana, k)
+    return resultado
 end;
 
 function ANNCrossValidation(topology::AbstractArray{<:Int,1},
