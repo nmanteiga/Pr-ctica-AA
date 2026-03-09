f41768ebcfd4d09470158386b6602b8c3892903f
Ansur López Braña <ansur.lopez.brana@udc.es>
2026-03-09 12:24:17 +0100

    tercera función ej 4

--- Entrega2/ejercicio4.jl
+++ Entrega2/ejercicio4.jl
@@ -40,8 +40,25 @@ function confusionMatrix(outputs::AbstractArray{Bool,2}, targets::AbstractArray{
         for i in 1:size(outputs, 2)
             _, _, sensibilidad[i], especificidad[i], vpp[i], vpn[i], F1[i], _ = confusionMatrix(vec(outputs[:, i]), vec(targets[:, i]))
         end
-
-
+        matriz = targets' * outputs
+        counts = vec(sum(targets, dims=1))
+        if weighted
+            total = sum(counts)
+            sensibilidadM = sum(counts .* sensibilidad) / total
+            especificidadM = sum(counts .* especificidad) / total
+            vppM = sum(counts .* vpp) / total
+            vpnM = sum(counts .* vpn) / total
+            F1M = sum(counts .* F1) / total
+        else
+            sensibilidadM = mean(sensibilidad)
+            especificidadM = mean(especificidad)
+            vppM = mean(vpp)
+            vpnM = mean(vpn)
+            F1M = mean(F1)
+        end
+        precision = accuracy(outputs, targets)
+        tasa_error = 1 - precision
+        return precision, tasa_error, sensibilidadM, especificidadM, vppM, vpnM, F1M, matriz
     end
 end;
 
